870a2bad9fdc968277f9c32cb59463db43aabf48
Sourabh Verma <sourabh.coder@gmail.com>
2017-08-13 17:01:50 +0530

    Update sample app

--- react-native-stateview-demo/App.js
+++ react-native-stateview-demo/App.js
@@ -1,22 +1,122 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    StyleSheet,
+    Button,
+    Text,
+    View,
+    Image,
+    FlatList,
+    TouchableOpacity,
+} from 'react-native';
 import StateView from 'react-native-stateview'
+import dishes from './dishes'
+
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <StateView style={styles.container} mode={StateView.Mode.loading}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </StateView>
-    );
-  }
+
+    _getRandomStateViewMode = () => {
+        switch (Math.floor(Math.random() * 3)) {
+            case 0:
+                return StateView.Mode.content;
+            case 1:
+                return StateView.Mode.error;
+            case 2:
+            default:
+                return StateView.Mode.placeholder;
+        }
+    };
+
+    _renderItem = ({item}) => {
+        return (
+            <TouchableOpacity>
+                <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', padding: 15}}>
+                    <Image source={item.icon} style={{width: 35, height: 35}} resizeMode="contain"/>
+                    <Text style={{marginLeft: 10, fontSize: 15}}>{item.name}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
+    _handleRefresh = (defaultDelay = 1000) => {
+        this.setState({refreshing: true});
+        setTimeout(() => {
+            this.setState({
+                mode: this._getRandomStateViewMode(),
+                refreshing: false
+            });
+        }, defaultDelay)
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: StateView.Mode.content,
+            refreshing: false
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <StateView
+                    style={styles.container}
+                    mode={this.state.mode}
+                    loadingTitle="Loading dishes..."
+                    placeholderImageRes={require("./icons/ic_pizza.png")}
+                    placeholderTitle="We're all out!"
+                    placeholderBody={`It looks like we all out of dishes.\nCome back tomorrow for a nice slice of pizza!`}
+                    placeholderButtonText="How about now?"
+                    placeholderButtonAction={() => this._handleRefresh(0)}
+                    errorImageRes={require("./icons/ic_salt.png")}
+                    errorTitle="Eh?! This shouldn't be happening"
+                    errorBody="Don't get so salty. We're working on it!"
+                    errorButtonText="FIX IT NOW!"
+                    errorButtonAction={() => this._handleRefresh(0)}
+                >
+                    <FlatList
+                        style={{flex: 1}}
+                        data={dishes}
+                        renderItem={this._renderItem}
+                        refreshing={this.state.refreshing}
+                        onRefresh={this._handleRefresh}
+                    />
+
+
+                </StateView>
+                <View style={styles.actionsContainer}>
+                    <Button
+                        textStyle={styles.actionTextStyle}
+                        onPress={() => this.setState({mode: StateView.Mode.loading})}
+                        title="Loading"/>
+                    <Button
+                        textStyle={styles.actionTextStyle}
+                        onPress={() => this.setState({mode: StateView.Mode.content})}
+                        title="Content"/>
+                    <Button
+                        textStyle={styles.actionTextStyle}
+                        onPress={() => this.setState({mode: StateView.Mode.error})}
+                        title="Error"/>
+                    <Button
+                        textStyle={styles.actionTextStyle}
+                        onPress={() => this.setState({mode: StateView.Mode.placeholder})}
+                        title="Placeholder"/>
+                </View>
+            </View>
+        );
+    }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+    },
+    actionsContainer: {
+        flexDirection: 'row'
+    },
+    actionTextStyle: {
+        fontSize: 8
+    }
 });
---
+++ react-native-stateview-demo/dishes.js
@@ -0,0 +1,229 @@
+export default [
+    {
+        key: 0,
+        icon: require('./icons/food_icons/ic_apple.png'),
+        name: "Apple"
+    },
+
+    {
+        key: 1,
+        icon: require('./icons/food_icons/ic_avocado.png'),
+        name: "Avocado"
+    },
+
+    {
+        key: 2,
+        icon: require('./icons/food_icons/ic_bell_pepper.png'),
+        name: "Bell Pepper"
+    },
+
+    {
+        key: 3,
+        icon: require('./icons/food_icons/ic_bento.png'),
+        name: "Bento"
+    },
+
+    {
+        key: 4,
+        icon: require('./icons/food_icons/ic_boiled_egg.png'),
+        name: "Boiled Eggs"
+    },
+
+    {
+        key: 5,
+        icon: require('./icons/food_icons/ic_bread.png'),
+        name: "Bread"
+    },
+
+    {
+        key: 6,
+        icon: require('./icons/food_icons/ic_burger.png'),
+        name: "Burger"
+    },
+
+    {
+        key: 7,
+        icon: require('./icons/food_icons/ic_candy.png'),
+        name: "Candy"
+    },
+
+    {
+        key: 8,
+        icon: require('./icons/food_icons/ic_cheese.png'),
+        name: "Cheese"
+    },
+
+    {
+        key: 9,
+        icon: require('./icons/food_icons/ic_chicken.png'),
+        name: "Chicken"
+    },
+
+    {
+        key: 10,
+        icon: require('./icons/food_icons/ic_clove.png'),
+        name: "Clove"
+    },
+
+    {
+        key: 11,
+        icon: require('./icons/food_icons/ic_corn.png'),
+        name: "Corn"
+    },
+
+    {
+        key: 12,
+        icon: require('./icons/food_icons/ic_crab.png'),
+        name: "Crab"
+    },
+
+    {
+        key: 13,
+        icon: require('./icons/food_icons/ic_cupcake.png'),
+        name: "Cupcake"
+    },
+
+    {
+        key: 14,
+        icon: require('./icons/food_icons/ic_egg.png'),
+        name: "Egg"
+    },
+
+    {
+        key: 15,
+        icon: require('./icons/food_icons/ic_fish.png'),
+        name: "Fish"
+    },
+
+    {
+        key: 16,
+        icon: require('./icons/food_icons/ic_fortune_cookie.png'),
+        name: "Fortune Cookie"
+    },
+
+    {
+        key: 17,
+        icon: require('./icons/food_icons/ic_fries.png'),
+        name: "Fries"
+    },
+
+    {
+        key: 18,
+        icon: require('./icons/food_icons/ic_grapes.png'),
+        name: "Grapes"
+    },
+
+    {
+        key: 19,
+        icon: require('./icons/food_icons/ic_hotdog.png'),
+        name: "Hot Dog"
+    },
+
+    {
+        key: 20,
+        icon: require('./icons/food_icons/ic_ice_cream_1.png'),
+        name: "Ice Cream"
+    },
+
+    {
+        key: 21,
+        icon: require('./icons/food_icons/ic_ice_cream_2.png'),
+        name: "Popsicle"
+    },
+
+    {
+        key: 22,
+        icon: require('./icons/food_icons/ic_mushroom.png'),
+        name: "Mushroom"
+    },
+
+    {
+        key: 23,
+        icon: require('./icons/food_icons/ic_noodles.png'),
+        name: "Noodles"
+    },
+
+    {
+        key: 24,
+        icon: require('./icons/food_icons/ic_onion.png'),
+        name: "Onion"
+    },
+
+    {
+        key: 25,
+        icon: require('./icons/food_icons/ic_pizza.png'),
+        name: "Pizza"
+    },
+
+    {
+        key: 26,
+        icon: require('./icons/food_icons/ic_pumpkin.png'),
+        name: "Pumpkin"
+    },
+
+    {
+        key: 27,
+        icon: require('./icons/food_icons/ic_rice.png'),
+        name: "Rice"
+    },
+
+    {
+        key: 28,
+        icon: require('./icons/food_icons/ic_salad.png'),
+        name: "Salad"
+    },
+
+    {
+        key: 29,
+        icon: require('./icons/food_icons/ic_salt.png'),
+        name: "Salt"
+    },
+
+    {
+        key: 30,
+        icon: require('./icons/food_icons/ic_sandwhich.png'),
+        name: "Sandwich"
+    },
+
+    {
+        key: 31,
+        icon: require('./icons/food_icons/ic_sausage.png'),
+        name: "Sausage"
+    },
+
+    {
+        key: 32,
+        icon: require('./icons/food_icons/ic_shrimp.png'),
+        name: "Shrimp"
+    },
+
+    {
+        key: 33,
+        icon: require('./icons/food_icons/ic_soup.png'),
+        name: "Soup"
+    },
+
+    {
+        key: 34,
+        icon: require('./icons/food_icons/ic_steak.png'),
+        name: "Steak"
+    },
+
+    {
+        key: 35,
+        icon: require('./icons/food_icons/ic_sushi.png'),
+        name: "Sushi"
+    },
+
+    {
+        key: 36,
+        icon: require('./icons/food_icons/ic_watermelon.png'),
+        name: "Watermelon"
+    },
+
+    {
+        key: 37,
+        icon: require('./icons/food_icons/ic_yogurt.png'),
+        name: "Yogurt"
+    }
+];
